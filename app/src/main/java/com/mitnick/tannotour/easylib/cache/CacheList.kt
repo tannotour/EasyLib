027cff6a06454e3a32bb99b50b4ba43cfe73ea49
mitnick <897922356@qq.com>
2017-12-21 17:21:12 +0800

    优化列表数据变化表示

--- app/src/main/java/com/mitnick/tannotour/easylib/cache/CacheList.kt
+++ app/src/main/java/com/mitnick/tannotour/easylib/cache/CacheList.kt
@@ -8,35 +8,79 @@ import java.util.concurrent.CopyOnWriteArrayList
  * Created by mitnick on 2017/12/16.
  * Description
  */
-abstract class CacheList<T>: CopyOnWriteArrayList<T>() {
+abstract class CacheList<T>(var maxSize: Int = 128): CopyOnWriteArrayList<T>() {
 
     /* 更新记录 */
     val changes: LinkedList<ChangeSet> = LinkedList()
 
     override fun add(element: T): Boolean {
+        if(size >= maxSize){
+            removeRange(0, maxSize/2)
+        }
         changes.add(ChangeSet(ChangeSet.TYPE.ADD, size))
         return super.add(element)
     }
 
     override fun add(index: Int, element: T) {
-        changes.add(ChangeSet(ChangeSet.TYPE.ADD, index))
-        super.add(index, element)
+        val position: Int
+        if(size >= maxSize){
+            removeRange(0, maxSize/2)
+            if(index < maxSize/2){
+                position = 0
+            }else{
+                position = minOf(index, size)
+            }
+        }else{
+            position = minOf(maxOf(index, 0), size)
+        }
+        changes.add(ChangeSet(ChangeSet.TYPE.ADD, position))
+        super.add(position, element)
+    }
+
+    fun addRange(index: Int, elements: List<T>){
+        val position = minOf(maxOf(index, 0), size)
+        elements.reversed().forEach {
+            super.add(position, it)
+        }
+        changes.add(ChangeSet(ChangeSet.TYPE.ADD_RANGE, position))
     }
 
     override fun set(index: Int, element: T): T {
-        changes.add(ChangeSet(ChangeSet.TYPE.SET, index))
+        changes.add(ChangeSet(ChangeSet.TYPE.SET, minOf(maxOf(index, 0), size)))
         return super.set(index, element)
     }
 
     override fun removeAt(index: Int): T {
-        changes.add(ChangeSet(ChangeSet.TYPE.REMOVE, index))
+        changes.add(ChangeSet(ChangeSet.TYPE.REMOVE, minOf(maxOf(index, 0), size)))
         return super.removeAt(index)
     }
 
     override fun remove(element: T): Boolean {
         val index = indexOf(element)
+        if(index < 0 || index >= size){
+            return false
+        }
         changes.add(ChangeSet(ChangeSet.TYPE.REMOVE, index))
         return super.remove(element)
+    }
+
+    fun removeRange(positionStart: Int, positionEnd: Int): Boolean{
+        val start = maxOf(positionStart, 0)
+        val end = minOf(positionEnd, size)
+        if(start < end){
+            return false
+        }
+        for (index in end..start step -1){
+            super.removeAt(index)
+        }
+        changes.add(ChangeSet(ChangeSet.TYPE.REMOVE_RANGE, start, end))
+        return true
+    }
+
+    override fun clear() {
+        changes.clear()
+        changes.add(ChangeSet(ChangeSet.TYPE.CLEAR))
+        super.clear()
     }
 
     fun clearRecord(){
--- app/src/main/java/com/mitnick/tannotour/easylib/cache/value/ChangeSet.kt
+++ app/src/main/java/com/mitnick/tannotour/easylib/cache/value/ChangeSet.kt
@@ -4,10 +4,13 @@ package com.mitnick.tannotour.easylib.cache.value
  * Created by mitnick on 2017/11/11.
  * Description
  */
-class ChangeSet(var type: TYPE, var position: Int){
+class ChangeSet(var type: TYPE, var positionStart: Int = -1, var positionEnd: Int = -1){
     enum class TYPE{
         ADD,
+        ADD_RANGE,
         SET,
-        REMOVE
+        REMOVE,
+        REMOVE_RANGE,
+        CLEAR
     }
 }
